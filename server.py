2bea265a98e1feb986e85ced9ef5bddd29dfd8d7
Luis Miguel de los Santos <84556152+luismigue432@users.noreply.github.com>
2024-09-29 09:58:23 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-import random as aleatorio
+import random as aleatorio #importamos la libreria random
 
-n1 = aleatorio.randint(0, 12)
-n2 = aleatorio.randint(0, 12)
+n1 = aleatorio.randint(0, 12) #generamos un numero aleatorio
+n2 = aleatorio.randint(0, 12) #generamos un numero aleatorio
 
-print(f"{n1} + {n2}=?")
+print(f"{n1} + {n2}=?") #imprimimos la suma
 
-n3 = int(input(""))
+n3 = int(input(""))  #ingresamos la suma
 
-if n1 + n2 == n3:
-    print("suma correcta")
+if n1 + n2 == n3: #comparamos la suma   
+    print("suma correcta") #imprimimos el resultado
 else:
-    print("suma incorrecta")
+    print("suma incorrecta") #imprimimos el resultado
+
 
     
